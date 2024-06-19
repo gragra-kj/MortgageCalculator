0612ac77d5cf5e031de21e2d561ba3cbef88b25b
gragra-kj <muthuigracia27@gmail.com>
2024-06-19 16:53:39 +0300

    use of loops

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,39 @@ public class Main {
     public static void main(String[] args) {
         final byte MONTH=12;
         final byte PERCENT=100;
+        int principal=0;
+        float monthlyRate=0;
+        int numberOfPayment=0;
         Scanner scanner=new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principal=scanner.nextInt();
-        System.out.print("Annual Interest Rate: ");
-        float rate=scanner.nextFloat();
-        float monthlyRate=rate / PERCENT / MONTH;
-        System.out.print("Period (Years): ");
-        int period=scanner.nextInt();
-        int numberOfPayment=period * MONTH;
+        while (true){
+            System.out.print("Principal: ");
+            principal=scanner.nextInt();
+            if (principal >= 1000 && principal<=1000000)
+                break;
+            System.out.println("Please enter a value between 1000 and 1000000");
+        }
+        while (true){
+            System.out.print("Annual Interest Rate: ");
+            float rate=scanner.nextFloat();
+            if (rate >0 && rate <=30){
+                monthlyRate=rate / PERCENT / MONTH;
+                break;
+            }else {
+                System.out.println("Enter a value between 1 and 30");
+            }
+
+        }
+        while (true){
+            System.out.print("Period (Years): ");
+            int period=scanner.nextInt();
+            if (period >1 && period <=30){
+                numberOfPayment=period * MONTH;
+                break;
+            }else {
+                System.out.println("Enter a value between 1 and 30");
+            }
+
+        }
 
         double mortgage= principal *((monthlyRate*Math.pow(1+monthlyRate,numberOfPayment))/(Math.pow(1+monthlyRate,numberOfPayment) -1));
         String mortgageFormat= NumberFormat.getCurrencyInstance().format(mortgage);
